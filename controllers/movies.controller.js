95c2ff62517a7055019da72a617ddac0f74d5960
Robert <robertpaul@quadkast.co.in>
2024-06-03 12:40:18 +0530

    model

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,9 +1,26 @@
+import Movie from "../modals/movie.modal.js"
+
 export const MovieIndex = (req, res) => {
     res.send("Get a movie")
 }
-export const MovieCreate = (req, res) => {
-    // res.send("Create a movie")
-    console.log(req.body) 
+
+export const MovieCreate = async (req, res) => {
+
+    // id,title,desc
+
+    // Validate your user
+
+    const newMovie = new Movie({
+        title: req.body.title,
+        desc: req.body.desc
+    })
+
+    try {
+        const movie = await newMovie.save();
+        return res.status(200).json(movie)
+    } catch (error) {
+        return res.status(400).json({ message: error.message })
+    }
 
     return res.json(req.body)
 }
